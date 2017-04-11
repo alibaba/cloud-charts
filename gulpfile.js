265644f256b75e821cbbc38e25bff78707853303
业枫 <zhaoye.zzy@alibaba-inc.com>
2017-04-11 20:07:36 +0800

    feat: 去除tnpm引用的scss编译

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const gulp = require('gulp');
-const sass = require('gulp-sass');
+// const sass = require('gulp-sass');
 const gutil = require('gulp-util');
 const webpack = require('webpack');
 const babel = require('gulp-babel');
@@ -96,9 +96,8 @@ gulp.task('build:demo', ['clean'], (cb) => {
 });
 
 gulp.task('build:lib', ['clean'], () => {
-    gutil.log('编译lib...');
     gulp.src(['src/**/*.less', 'src/**/*.scss', 'src/**/*.scssm', 'src/**/*.lessm', 'src/**/*.cssm'])
-        .pipe(sass().on('error', sass.logError))
+        // .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('lib'));
 
     return gulp.src('src/**/*.js?(x)')
