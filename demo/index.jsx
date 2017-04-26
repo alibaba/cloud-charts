2ab67001556fb62d88e3f83fda63c769b9e9f116
业枫 <zhaoye.zzy@alibaba-inc.com>
2017-04-26 15:07:46 +0800

    docs: 添加demo页面文档链接，方便查看

--- demo/index.jsx
+++ demo/index.jsx
@@ -112,47 +112,63 @@ class App extends React.Component {
     return (
       <div className="p2-chart-demo">
         <h3>示例</h3>
+        <h4>IconLinkButtonMini - <a target="_blank" href="http://gitlab.alibaba-inc.com/ls/p2widgets/tree/master/src/components/iconLinkButtonMini">文档</a></h4>
         <Row>
           <Col span="4">
             <IconLinkButtonMini title="OSD" 
             subTitle="db.zhangbei.na61.1" 
             url="http://aisc.alibaba.net/docs/components/icon/"/>
           </Col>
+        </Row>
+        <h4>OnlyLiteralMini - <a target="_blank" href="http://gitlab.alibaba-inc.com/ls/p2widgets/tree/master/src/components/onlyLiteralMini">文档</a></h4>
+        <Row>
           <Col span="9">
             <Panel>
               <OnlyLiteralMini dataSource={healthyStatusData}/>
             </Panel>
           </Col>
         </Row>
+        <h4>PieChartTitleMini - <a target="_blank" href="http://gitlab.alibaba-inc.com/ls/p2widgets/tree/master/src/components/pieChartTitleMini">文档</a></h4>
         <Row>
           <Col span="9">
             <Panel>
               <PieChartTitleMini dataSource={ringRroportionData}/>
             </Panel>
           </Col>
+        </Row>
+        <h4>BarChartDetailsMini - <a target="_blank" href="http://gitlab.alibaba-inc.com/ls/p2widgets/tree/master/src/components/barChartDetailsMini">文档</a></h4>
+        <Row>
           <Col span="9">
             <Panel>
               <BarChartDetailsMini dataSource={loadProgressData}/>
             </Panel>
           </Col>
         </Row>
+        <h4>OnlyDetailsLiteralLattice - <a target="_blank" href="http://gitlab.alibaba-inc.com/ls/p2widgets/tree/master/src/components/onlyDetailsLiteralLattice">文档</a></h4>
         <Row>
           <Col span="9">
             <Panel title="Device IO">
               <OnlyDetailsLiteralLattice dataSource={onlyDetailsLiteralLatticeData} row={2} col={3} details={onlyDetailsLiteralLatticeDetails}/>
             </Panel>
           </Col>
+        </Row>
+        <h4>IconTitleMini - <a target="_blank" href="http://gitlab.alibaba-inc.com/ls/p2widgets/tree/master/src/components/iconTitleMini">文档</a></h4>
+        <Row>
           <Col span="6">
             <Panel>
               <IconTitleMini dataSource={iconTitleMiniData}/>
             </Panel>
           </Col>
+        </Row>
+        <h4>OnlyDetailsLiteralRowsMini - <a target="_blank" href="http://gitlab.alibaba-inc.com/ls/p2widgets/tree/master/src/components/onlyDetailsLiteralRowsMini">文档</a></h4>
+        <Row>
           <Col span="6">
             <Panel>
               <OnlyDetailsLiteralRowsMini dataSource={onlyDetailsLiteralRowsMiniData}/>
             </Panel>
           </Col>
         </Row>
+        <h4>CapacityIndex</h4>
         <Row>
           <Col span="10">
             <Panel title="机器数据指标" tabMode>
@@ -167,6 +183,9 @@ class App extends React.Component {
               </Item>
             </Panel>
           </Col>
+        </Row>
+        <h4>StateIndex</h4>
+        <Row>
           <Col span="14">
             <Panel title="OSD指标">
               <Item>
@@ -181,12 +200,15 @@ class App extends React.Component {
             </Panel>
           </Col>
         </Row>
+        <h4>RankList</h4>
         <Row>
           <Col span="24">
             <Tops />
           </Col>
         </Row>
+        <h4>TopList - <a target="_blank" href="http://gitlab.alibaba-inc.com/ls/p2widgets/tree/master/src/components/topList">文档</a></h4>
         <TopList />
+        <h4>TableList</h4>
         <Row>
           <Col span="24">
             <Clusters />
