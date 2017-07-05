216ac9d51f93111e192aad08813f2ce6fd7ff762
笑斌 <xinlei.sxl@alibaba-inc.com>
2017-07-05 16:34:27 +0800

    feat: 十字方式排列

--- components/wcontainer/demo/number.md
+++ components/wcontainer/demo/number.md
@@ -24,33 +24,59 @@ import {
 const { Row, Col } = Grid;
 
 ReactDOM.render(
-    <Row>
-      <Col span="4">
-        <Wcontainer title={false} height={86}>
-          <Wnumber bottomTitle="CPU" >1</Wnumber>
-          <Wicon type="monitor" size="big" />
-        </Wcontainer>
-      </Col>
-      <Col span="4">
-        <Wcontainer title={false} height={86}>
-          <Wnumber bottomTitle="CPU" >12</Wnumber>
-          <Wicon type="monitor" size="big" />
-        </Wcontainer>
-      </Col>
-      <Col span="4">
-        <Wcontainer title={false} height={86}>
-          <Wnumber bottomTitle="CPU" >123</Wnumber>
-          <Wicon type="monitor" size="big" />
-        </Wcontainer>
-      </Col>
-      <Col span="12">
-        <Wcontainer title={false} height={86}>
-          <Wnumber rightTitle="物理机总量" >123</Wnumber>
-          <Wcontainer.divider />
-          <Wnumber rightTitle="本月数量" >123</Wnumber>
-          <Wnumber rightTitle="较去年同期" numberTrend="raise">1,234</Wnumber>
-        </Wcontainer>
-      </Col>
-    </Row>,
+    <div>
+      <Row>
+        <Col span="4">
+          <Wcontainer title={false} height={86}>
+            <Wnumber bottomTitle="CPU" >1</Wnumber>
+            <Wicon type="monitor" size="big" />
+          </Wcontainer>
+        </Col>
+        <Col span="4">
+          <Wcontainer title={false} height={86}>
+            <Wnumber bottomTitle="CPU" >12</Wnumber>
+            <Wicon type="monitor" size="big" />
+          </Wcontainer>
+        </Col>
+        <Col span="4">
+          <Wcontainer title={false} height={86}>
+            <Wnumber bottomTitle="CPU" >123</Wnumber>
+            <Wicon type="monitor" size="big" />
+          </Wcontainer>
+        </Col>
+        <Col span="12">
+          <Wcontainer title={false} height={86}>
+            <Wnumber rightTitle="物理机总量" >1</Wnumber>
+            <Wnumber rightTitle="本月数量" >1</Wnumber>
+            <Wcontainer.divider />
+            <Wnumber rightTitle="较去年同期" numberTrend="drop">1</Wnumber>
+            <Wnumber rightTitle="较去年同期" numberTrend="drop">1</Wnumber>
+          </Wcontainer>
+        </Col>
+      </Row>
+      <Row>
+        <Col span="12">
+          <Wcontainer title={false} height={192} arrange="cross" title="233">
+            <Wnumber bottomTitle="物理机总量" >21</Wnumber>
+            <Wnumber bottomTitle="本月数量" >1,024</Wnumber>
+            <Wnumber bottomTitle="本月数量" >1</Wnumber>
+            
+            <Wcontainer.divider />
+            <Wnumber bottomTitle="本月数量" >1</Wnumber>
+            <Wnumber bottomTitle="较去年同期" numberTrend="drop">100</Wnumber>
+            <Wnumber bottomTitle="较去年同期" numberTrend="raise">1</Wnumber>
+          </Wcontainer>
+        </Col>
+        <Col span="12">
+          <Wcontainer title={false} height={86}>
+            <Wnumber rightTitle="物理机总量" >1</Wnumber>
+            <Wnumber rightTitle="本月数量" >1</Wnumber>
+            <Wcontainer.divider />
+            <Wnumber rightTitle="较去年同期" numberTrend="drop">1</Wnumber>
+            <Wnumber rightTitle="较去年同期" numberTrend="drop">1</Wnumber>
+          </Wcontainer>
+        </Col>
+      </Row>
+    </div>,
 mountNode);
 ````
--- components/wcontainer/index.jsx
+++ components/wcontainer/index.jsx
@@ -16,6 +16,10 @@ export default class Wcontainer extends React.Component {
     super(props);
   }
 
+  static defaultProps = {
+    arrange: 'normal'
+  }
+
   renderTitle() {
     return (
       <div className={`${prefix}-title`}>
@@ -26,37 +30,109 @@ export default class Wcontainer extends React.Component {
     );
   }
 
-  render() {
-    let { height = 0, d } = this.props;
-    const styleObj = {
-      'min-height': height + 'px',
+  renderMainNormal() {
+    let { height = 0 } = this.props;
+
+    let rowHeight = height;
+    if ( this.props.title ) {
+      //标题字体14px ,距下面有10px宽度
+      rowHeight = rowHeight - 14 - 10;
+    }
+
+    const containerClasses = classNames({
+      [`${prefix}-main`]: true,
+    });
+
+    return (
+      <div className={containerClasses}>
+          <Row align="center" style={{
+            lineHeight: rowHeight + 'px',
+            height: rowHeight + 'px'
+          }}>
+          {
+            React.Children.map(this.props.children, (child, i) => {
+              if (child.type.name === 'Wicon') {
+                return <Col fixedSpan="2">{child}</Col>
+              }
+              if (child.type.name === 'Divider') {
+                return <Col fixedSpan="1">{child}</Col>
+              }
+              return (
+                <Col>{child}</Col>
+              )
+            })
+          }
+          </Row>
+      </div>
+    );
+  }
+
+  renderMainCross() {
+    let {height = 0 } = this.props;
+
+    let rowHeight = height;
+    if ( this.props.title ) {
+      //标题字体14px ,距下面有10px宽度
+      rowHeight = rowHeight - 14 - 10 - 20;
+    }
+    const rowStyle = {
+      lineHeight: rowHeight + 'px',
+      height: rowHeight + 'px'
     };
 
     const containerClasses = classNames({
       [`${prefix}-main`]: true,
-    })
+      [`${prefix}-cross`]: true
+    });
 
+    let maxColPerRow = 0;
+    let currentColPerRow = 0;
+    // 计算栅格的ColSpan
+    React.Children.forEach(this.props.children, (child, i) => {
+      if (child.type.name !== 'Divider') {
+        currentColPerRow += 1;
+      } else {
+        if (currentColPerRow > maxColPerRow) {
+          maxColPerRow = currentColPerRow;
+        }
+      }
+    });
+    const ColPerRow = ~~(24 / maxColPerRow);
+    function chunks(arr, maxSpan) {
+      const rs = [];
+      let oneRow = [];
+      React.Children.forEach(arr, (child, i) => {
+        if(child.type.name === 'Divider') {
+          rs.push(<Row align="center">{oneRow}</Row>);
+          oneRow = [];
+        } else if (i === arr.length - 1) {
+          oneRow.push(<Col span={ColPerRow}>{child}</Col>);
+          rs.push(<Row align="center">{oneRow}</Row>);
+        } else {
+          oneRow.push(<Col span={ColPerRow}>{child}</Col>);
+        }
+      });
+      return rs;
+    }
 
     return (
-      <div className={`${prefix}`} style={styleObj}>
-        {this.props.title && this.renderTitle()}
-        <div className={containerClasses}>
-           <Row align="center">
-            {
-              React.Children.map(this.props.children, (child, i) => {
-                if (child.type.name === 'Wicon') {
-                  return <Col fixedSpan="2">{child}</Col>
-                }
-                if (child.type.name === 'Divider') {
-                  return <Col fixedSpan="1">{child}</Col>
-                }
-                return (
-                  <Col>{child}</Col>
-                )
-              })
-            }
-           </Row>
-        </div>
+      <div className={containerClasses}>
+        <div className={`${prefix}-multi-row-container`} style={rowStyle}>{chunks(this.props.children, ColPerRow)}</div>
+      </div>
+    );
+  }
+
+  render() {
+    const { height = 0, arrange, title } = this.props;
+
+    return (
+      <div className={`${prefix}`} style={{
+        minHeight: height + 'px',
+        height: height + 'px'
+      }}>
+      {title && this.renderTitle()}
+      {arrange === 'normal' && this.renderMainNormal()}
+      {arrange === 'cross' && this.renderMainCross()}
       </div>
     );
   }
