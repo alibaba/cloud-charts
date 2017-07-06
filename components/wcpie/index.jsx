1a686051b806731e4a3de9cb5f6f33d372888856
笑斌 <xinlei.sxl@alibaba-inc.com>
2017-07-06 14:27:23 +0800

    feat: 饼图组件

---
+++ components/wcpie/index.jsx
@@ -0,0 +1,18 @@
+'use strict';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import './index.scss';
+
+const prefix = 'aisc-wcpie';
+
+export default class WCpie extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return ()
+  }
+}
