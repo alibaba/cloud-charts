49677565fcdce922b44a06b29dc2b6db49bacfb8
绯玉 <lengqing.lq@alibaba-inc.com>
2017-08-07 17:43:12 +0800

    feat: 颜色统一

--- components/chartCommon/colors.js
+++ components/chartCommon/colors.js
@@ -1,23 +1,19 @@
 'use strict';
 
 const colors = [
-  '#389BFF',
-  '#F2A72D',
-  '#8CD123',
-  '#E1575D',
+  '#2889EC',
+  '#F6A71F',
+  '#EF5350',
+  '#4AD051',
   '#8B73CC',
-  '#FCEF00',
-  '#4B53BA',
+  '#0F1BB3',
+
   '#429588',
   '#735546',
   '#D42762',
   '#673EBC',
   '#65DBFF',
-  '#5ABCD6',
-  '#64AE45',
-  '#CBDB00',
-  '#F1C000',
-  '#EA5900'
+  '#5ABCD6'
 ];
 
 export default colors;
