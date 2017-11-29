f3a7110234e352f52ce5713b3ddc09eec1f67469
业枫 <zhaoye.zzy@alibaba-inc.com>
2017-11-29 11:38:27 +0800

    fix: 修复js scss字体变量报错问题

--- components/variables.js
+++ components/variables.js
@@ -1,4 +1,6 @@
 export colors from '!!sass-variable-loader!@alife/aisc-core/scss/variables/_color.scss';
-import fonts from '!!sass-variable-loader!@alife/aisc-core/scss/variables/_font.scss';
 
+import fonts from '!!sass-variable-loader!@alife/aisc-core/scss/variables/_font.scss';
 fonts['fontFamilyNumber'] = "DINMediumNumber, " + fonts.fontFamilyBase;
+
+export { fonts };
