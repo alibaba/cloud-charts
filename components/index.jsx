0334ca6b678f27f4e93d611025f044ed7da65cb0
业枫 <zhaoye.zzy@alibaba-inc.com>
2017-12-06 18:02:43 +0800

    refactor: 精简代码

--- components/index.jsx
+++ components/index.jsx
@@ -8,43 +8,43 @@ export Wcircle from './wcircle/index';
 export Wminicontainer from './wminicontainer/index';
 export Wshoot from './wshoot/index';
 
-import * as g2Map from './g2Map';
-import * as highchartsMap from './highchartsMap';
-
-export const WG2Line = g2Map.WG2Line;
-export const WG2Bar = g2Map.WG2Bar;
-export const WG2Pie = g2Map.WG2Pie;
-export const WG2Map = g2Map.WG2Map;
-export const WG2Custom = g2Map.WG2Custom;
-
-export const WHighLine = highchartsMap.WHighLine;
-export const WHighBar = highchartsMap.WHighBar;
-export const WHighPie = highchartsMap.WHighPie;
-export const WHighLineBar = highchartsMap.WHighLineBar;
-
-//根据设置的library名字暴露默认的图表组件
-const chartLibraryName = 'Highcharts';
-const chartMap = {
-  Highcharts: {
-    line: highchartsMap.WHighLine,
-    bar: highchartsMap.WHighBar,
-    pie: highchartsMap.WHighPie,
-    linebar: highchartsMap.WHighLineBar,
-    map: g2Map.WG2Map,
-    custom: g2Map.WG2Custom
-  },
-  G2: {
-    line: g2Map.WG2Line,
-    bar: g2Map.WG2Bar,
-    pie: g2Map.WG2Pie,
-    linebar: highchartsMap.WHighLineBar,
-    map: g2Map.WG2Map,
-    custom: g2Map.WG2Custom
-  }
-};
-export const Wline = chartMap[chartLibraryName].line;
-export const Wbar = chartMap[chartLibraryName].bar;
-export const Wpie = chartMap[chartLibraryName].pie;
-export const Wlinebar = chartMap[chartLibraryName].linebar;
-export const Wmap = chartMap[chartLibraryName].map;
-export const Wcustom = chartMap[chartLibraryName].custom;
+export * as g2Map from './g2Map';
+// import * as highchartsMap from './highchartsMap';
+//
+// export const WG2Line = g2Map.WG2Line;
+// export const WG2Bar = g2Map.WG2Bar;
+// export const WG2Pie = g2Map.WG2Pie;
+// export const WG2Map = g2Map.WG2Map;
+// export const WG2Custom = g2Map.WG2Custom;
+//
+// export const WHighLine = highchartsMap.WHighLine;
+// export const WHighBar = highchartsMap.WHighBar;
+// export const WHighPie = highchartsMap.WHighPie;
+// export const WHighLineBar = highchartsMap.WHighLineBar;
+//
+// //根据设置的library名字暴露默认的图表组件
+// const chartLibraryName = 'G2';
+// const chartMap = {
+//   // Highcharts: {
+//   //   line: highchartsMap.WHighLine,
+//   //   bar: highchartsMap.WHighBar,
+//   //   pie: highchartsMap.WHighPie,
+//   //   linebar: highchartsMap.WHighLineBar,
+//   //   map: g2Map.WG2Map,
+//   //   custom: g2Map.WG2Custom
+//   // },
+//   G2: {
+//     line: g2Map.WG2Line,
+//     bar: g2Map.WG2Bar,
+//     pie: g2Map.WG2Pie,
+//     linebar: highchartsMap.WHighLineBar,
+//     map: g2Map.WG2Map,
+//     custom: g2Map.WG2Custom
+//   }
+// };
+// export const Wline = chartMap[chartLibraryName].line;
+// export const Wbar = chartMap[chartLibraryName].bar;
+// export const Wpie = chartMap[chartLibraryName].pie;
+// export const Wlinebar = chartMap[chartLibraryName].linebar;
+// export const Wmap = chartMap[chartLibraryName].map;
+// export const Wcustom = chartMap[chartLibraryName].custom;
--- oldComponents/charts/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import AiscP2charts from '@alife/aisc-p2charts';
-import '@alife/aisc-p2charts/lib/index.scss';
-
-export default AiscP2charts;
