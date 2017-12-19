7116951872eec6e29713dbb9e5978be22211947a
徐跃 <shuyang.nsy@alibaba-inc.com>
2017-12-19 10:43:43 +0800

    feat：桑基图

--- components/g2Map.js
+++ components/g2Map.js
@@ -6,6 +6,7 @@ import G2Bar from './wbar/G2Bar';
 import G2Pie from './wpie/G2Pie';
 import G2Map from './wmap/G2Map';
 import G2Custom from './wcustom/G2Custom';
+import G2Sankey from './wsankey/G2Sankey';
 
 //暴露所有基础图表
 export const WG2Line = g2Factory('G2Line', G2Line);
@@ -13,6 +14,7 @@ export const WG2Bar = g2Factory('G2Bar', G2Bar);
 export const WG2Pie = g2Factory('G2Pie', G2Pie);
 export const WG2Map = g2Factory('G2Map', G2Map, false);
 export const WG2Custom = g2Factory('G2Custom', G2Custom);
+export const WG2Sankey = g2Factory('G2Sankey', G2Sankey,false);
 
 //暴露基础图表库
 export { G2 }
@@ -22,3 +24,4 @@ export const Wbar = WG2Bar;
 export const Wpie = WG2Pie;
 export const Wmap = WG2Map;
 export const Wcustom = WG2Custom;
+export const WSankey = WG2Sankey;
---
+++ components/wsankey/G2Sankey.js
@@ -0,0 +1,89 @@
+'use strict';
+
+import COLORS from '../chartCommon/colors';
+import merge from '../utils/merge';
+import G2 from '@antv/g2';
+import {DataSet} from '@antv/data-set';
+
+const Util = G2.Util;
+import './G2Sankey.scss';
+
+let defaultConfig = {
+  padding: [32, 5, 32, 45],
+  legend: {
+    align: 'left',
+    nameFormatter: null, //可以强制覆盖，手动设置label
+  },
+  tooltip: {
+    showTitle: false,
+    formatter: null
+  },
+  textStyle: {
+    fill: '#545454',
+    textAlign: 'start'
+  }
+};
+
+export default {
+  beforeInit(props) {
+    const {config, plotCfg} = props;
+    plotCfg.margin = config.padding || defaultConfig.padding;
+    return props;
+  },
+  init(chart, userConfig, data) {
+    // console.log(chart, userConfig, data)
+    const config = merge({}, defaultConfig, userConfig);
+    const ds = new DataSet();
+    const dv = ds.createView().source(data, {
+      type: 'graph',
+      edges: d => d.links
+    });
+    dv.transform({
+      type: 'diagram.sankey'
+    });
+
+    chart.legend(config.legend);
+    chart.tooltip(config.tooltip);
+    chart.axis(false);
+    chart.scale({
+      x: {sync: true},
+      y: {sync: true}
+    });
+
+    // edge view
+    const edgeView = chart.view();
+    edgeView.source(dv.edges);
+    edgeView.edge()
+      .position('x*y')
+      .shape('arc')
+      .color('#bbb')
+      .opacity(0.6)
+      .tooltip('target*source*value', config.tooltip.formatter);
+
+    // node view
+    const nodeView = chart.view();
+    nodeView.source(dv.nodes);
+    nodeView.polygon()
+      .position('x*y') // nodes数据的x、y由layout方法计算得出
+      .color('name')
+      .label('name', {
+        textStyle: config.textStyle,
+        offset: 0,
+        formatter: val => {
+          return '  ' + val;
+        }
+      })
+      .tooltip(false)
+      .style({
+        stroke: '#ccc'
+      });
+    chart.render();
+
+
+  }
+};
+
+function getLegendNode(target) {
+  if (target.tagName === 'LI') return target;
+  else return target.parentNode;
+}
