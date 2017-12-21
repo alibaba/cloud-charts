4bc52e31019706aa31dc67f024ed2269e93e0d30
lianzhao <lianzhao.wj@alibaba-inc.com>
2017-12-21 11:22:30 +0800

    feat:散点图文档完善

--- components/wscatter/G2Scatter.js
+++ components/wscatter/G2Scatter.js
@@ -3,34 +3,28 @@
 // 引入所需要的库和样式
 import G2 from '@antv/g2';
 import merge from '../utils/merge';
-import { g2LegendFilter } from '../chartCommon/common';
+import { color, fonts, size } from '../variables';
 import './G2Scatter.scss';
-
-const Util = G2.Util;
 
 // 建议将默认配置放在外层，方便后续维护
 const defaultConfig = {
   padding: [32, 5, 32, 45],
   colors: [],
-  width: 1200,
-  height: 800,
-  xAxis: { index: 2, type: 'datetime' },
-  yAxis: {
-    index: 0,
-    alias: '国家/地区'
+  xAxis: {
+    type: 'linear',
+    mask: 'YYYY-MM-DD HH:mm:ss'
   },
-  alias: ['国家/地区', '人均寿命', '人均国内生产总值', '人口总数']
+  yAxis: {
+    min: 0
+  },
+  tooltip: true,
+  legend: true
 };
 
-const colorMap = {
-  Asia: '#2889EC',
-  Americas: '#F6A71F',
-  Europe: '#EF5350',
-  Oceania: '#4AD051',
-  Africa: '#8B73CC'
-};
+const colorMap = ['#2889EC', '#F6A71F', '#EF5350', '#4AD051', '#8B73CC'];
 
 const setAxis = (chart, config) => {
+  console.log(config, 'xxxxxxxxxx');
   const { xFormator } = config;
 
   chart.axis('x', {
@@ -42,7 +36,8 @@ const setAxis = (chart, config) => {
       textAlign: 'center', // 文本对齐方向，可取值为： start middle end
       fill: '#999', // 文本的颜色
       fontSize: '12' // 文本大小
-    }
+    },
+    formatter: xFormator
   });
   chart.axis('y', {
     tickLine: null,
@@ -70,7 +65,31 @@ const setAxis = (chart, config) => {
 };
 
 const setSource = (chart, config, data) => {
-  chart.source(data);
+  chart.source(data, { x: config.xAxis, y: config.yAxis });
+};
+
+const chartRender = (chart, config) => {
+  const index = 0;
+  const typeArr = [];
+
+  chart
+    .point()
+    .color('type', (val) => {
+      let curIndex;
+      if (!typeArr.includes(val)) {
+        curIndex = typeArr.length;
+        typeArr.push(val);
+      } else {
+        curIndex = typeArr.indexOf(val);
+      }
+
+      return colorMap[curIndex];
+    })
+    .position('x*y')
+    .size(4)
+    .shape('circle');
+
+  chart.render();
 };
 
 export default {
@@ -78,116 +97,129 @@ export default {
     const { config } = props;
 
     return Object.assign({}, props, {
-      padding: props.padding || config.padding || defaultConfig.padding,
-      tooltip: true,
-      legend: true
+      padding: props.padding || config.padding || defaultConfig.padding
     });
   },
-  init(chart, userConfig, data) {
+  init(chart, userConfig, data, rawData) {
     const config = merge({}, defaultConfig, userConfig);
-    // 设置数据展示
     setSource(chart, config, data);
 
-    // 设置轴
     setAxis(chart, config);
 
-    // 设置tooltip
     setToolTip(chart, config);
 
-    chart
-      .point()
-      .position('x*y')
-      .size(4)
-      .shape('circle');
+    setLegend(chart, config, rawData, this.chartDom);
 
-    chart.render();
-
-    // 设置图例;
-    setLegend(chart, config);
-  },
-  // 自定义更新数据函数，常用于多view的情况更新数据
-  changeData(chart, newConfig, newData) {},
-  // 自定义更新大小函数
-  changeSize(chart, newConfig, newWidth, newHeight) {},
-  // 自定义销毁函数
-  destroy(chart) {}
+    chartRender(chart, config);
+  }
 };
 
-// 从其他图表复制，待改动
-const setLegend = (chart, config) => {
+const setLegend = (chart, config, rawData, chartNode) => {
   if (config.legend) {
-    const id = chart._attrs.id;
-    const chartNode = document.getElementById(id);
-    chartNode.style.position = 'relative';
-    const geom = chart.getGeoms()[0]; // 获取所有的图形
-    const items = geom.getData(); // 获取图形对应的数据
-    const stash = {};
-
-    const ulNode = document.createElement('ul');
-    ulNode.classList.add('ac-line-legend');
-    // ulNode.style.top = config.padding[0] + 'px';
-    if (config.legend.align === 'right') {
-      ulNode.style.right = `${config.padding[1]}px`;
-    } else {
-      ulNode.style.left = `${5}px`;
-    }
-    ulNode.innerHTML = '';
-    for (let i = 0, l = items.length; i < l; i++) {
-      const item = items[i];
-      const itemData = item._origin;
-      const color = item.color;
-      const type = itemData[0].type;
-      const name = itemData.name;
-      const value = itemData.value;
-
-      const typeFormatter = config.legend.nameFormatter ? config.legend.nameFormatter(type, item, i) : type;
-
-      const liHtml = `<li class="item" data-id="${type}"><i class="dot" style="background:${color};"></i><span>${typeFormatter}</span></li>`;
-      ulNode.innerHTML += liHtml;
-      chartNode.appendChild(ulNode);
-
-      stash[type] = {
-        item,
-        color,
-        name: type,
-        isChecked: true,
-        index: i
-      };
-    }
-    const dotDom = chartNode.getElementsByClassName('dot');
-    Array.prototype.forEach.call(ulNode.querySelectorAll('li'), (item) => {
-      item.addEventListener('click', (e) => {
-        const node = getLegendNode(e.target);
-        const type = node.getAttribute('data-id');
-        g2LegendFilter(type, stash, Util, dotDom, chart);
-      });
+    chart.legend({
+      useHtml: true,
+      title: null,
+      position: 'top',
+      containerTpl:
+        '<div class="g2-legend" style="position:absolute;top:0;right:60px;width:auto;">' +
+        '<h4 class="g2-legend-title"></h4>' +
+        '<ul class="g2-legend-list" style="list-style-type:none;margin:0;padding:0;"></ul>' +
+        '</div>',
+      itemTpl: (value, color, checked, index) => {
+        const item = (rawData && rawData[index]) || {};
+        const result = config.legend.nameFormatter
+          ? config.legend.nameFormatter(
+              value,
+            {
+              ...item,
+              color,
+              checked
+            },
+              index
+            )
+          : value;
+        return `${'<li class="g2-legend-list-item item-{index} {checked}" data-color="{originColor}" data-value="{originValue}">' +
+          '<i class="g2-legend-marker" style="background-color:{color};"></i>' +
+          '<span class="g2-legend-text">'}${result}</span></li>`;
+      },
+      'g2-legend': {
+        position: 'absolute',
+        overflow: 'auto',
+        fontFamily: fonts.fontFamilyBase,
+        fontSize: fonts.fontSizeBaseCaption,
+        lineHeight: fonts.fontSizeBaseCaption,
+        color: color.colorText14,
+        left: '20px',
+        top: 0
+      },
+      'g2-legend-list': {},
+      'g2-legend-list-item': {
+        marginBottom: size.s3,
+        marginRight: size.s3
+      },
+      'g2-legend-marker': {
+        width: '6px',
+        height: '6px',
+        marginRight: size.s1
+      }
     });
+
+    G2.DomUtil.requestAnimationFrame(() => {
+      const legendDom = chartNode.querySelector('.g2-legend');
+      if (config.legend.align === 'right') {
+        legendDom && legendDom.classList.add('legend-align-right');
+      } else {
+        legendDom && legendDom.classList.add('legend-align-left');
+      }
+    });
+  } else {
+    chart.legend(false);
   }
 };
 
 const setToolTip = (chart, config) => {
   if (config.tooltip) {
     const tooltipCfg = {
+      // crosshairs 空对象不可省略，否则在混合图表中会没有crosshairs line
+      crosshairs: null,
       custom: true,
-      offset: 8,
-      crosshairs: {
-        type: 'y' // 启用水平方向的辅助线
+      containerTpl: '<div class="g2-tooltip">' + '<p class="g2-tooltip-title">{name}</p>' + '<div class="g2-tooltip-list"></div>' + '</div>', // tooltip的外层模板
+      itemTpl: '<div class="g2-tooltip-list-item"><span style="color:{color}"></span><span>{value}</span></div>', // 支持的字段 index,color,name,value
+      'g2-tooltip': {
+        position: 'absolute',
+        visibility: 'hidden',
+        border: '1px solid #efefef',
+        backgroundColor: 'white',
+        padding: '12px',
+        transition: 'top 200ms,left 200ms',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        color: '#333'
+      }, // 设置 tooltip 的 css 样式
+      'g2-tooltip-title': {
+        color: '#333',
+        marginRight: '12px',
+        fontSize: '12px'
       },
-      crossLine: null,
-      padding: [12, 12, 12, 12],
-      labels: {
-        autoRotate: false
-      },
-      html: '<div class="ac-tooltip" style="position:absolute;visibility: hidden;"><h4 class="ac-title"></h4><ul class="ac-list"></ul></div>',
-      itemTpl: '<li><i style="background-color:{color}"></i>{name}<span>{value}</span></li>'
+      'g2-tooltip-list-item': {
+        marginTop: 0
+      }
     };
-    chart.tooltip(true, tooltipCfg);
+    chart.tooltip(tooltipCfg);
     if (config.tooltip.titleFormatter || config.tooltip.nameFormatter || config.tooltip.valueFormatter) {
-      chart.on('tooltipchange', (ev) => {
+      chart.on('tooltip:change', (ev) => {
+        if (config.tooltip.titleFormatter) {
+          ev.items[0].title = config.tooltip.titleFormatter(ev.items[0].title, ev.items);
+        }
+
         ev.items.forEach((item, index) => {
-          item.title = config.tooltip.titleFormatter ? config.tooltip.titleFormatter(item.title, ev.items, index, item.point._origin) : item.title;
-          item.value = config.tooltip.valueFormatter ? config.tooltip.valueFormatter(item.value, ev.items, index, item.point._origin) : item.value;
-          item.name = config.tooltip.nameFormatter ? config.tooltip.nameFormatter(item.name, ev.items, index, item.point._origin) : item.name;
+          if (config.tooltip.valueFormatter) {
+            item.value = config.tooltip.valueFormatter(item.value, ev.items, index, item.point._origin);
+          }
+          if (config.tooltip.nameFormatter) {
+            item.name = config.tooltip.nameFormatter(item.name, ev.items, index, item.point._origin);
+          }
         });
       });
     }
--- components/wscatter/index.zh-CN.md
+++ components/wscatter/index.zh-CN.md
@@ -20,3 +20,58 @@ cols: 1
 ### 线图配置及数据格式
 
 * config:
+
+```javascript
+  {
+    xAxis: {
+      type: 'linear', //X轴类型，默认为线性“linear”，还可以设置为时间“time”
+      mask: '%m-%d', //时间格式化，当X轴类型为“datetime”时，此字段生效
+      labelFormatter: null, //自定义格式化，默认为null。设置值后dateFormatter失效
+    },
+    // yAxis 设置为数组形式可变为双轴模式
+    yAxis: {
+      labelFormatter: null, //自定义格式化，默认为null
+      max: null, //Y轴可见最大值，默认为自动计算。label最大不一定会显示此值，还与间隔有关
+      min: null,  //Y轴可见最小值，默认为自动计算，一般为0
+    },
+    legend: { //默认显示，设置为false时，表示不显示
+      nameFormatter: null, // 图例文字格式化函数。参数：name, data, index
+    },
+    tooltip: { //默认显示，设置为false时，表示不显示
+      titleFormatter: null, // tooltip标题格式化函数。参数：title
+      nameFormatter: null, // tooltip name格式化函数。参数：name, data, index, record
+      valueFormatter: null, // tooltip value格式化函数。参数：value, data, index, record
+    },
+    labels: { //默认为false，表示不显示
+      labelFormatter: null, // 图例文字格式化函数。参数：value
+    },
+    colors: ['#389BFF','#F2A72D','#8CD123',...], //线条配色，默认有17种颜色。不建议修改
+    padding: [12, 0, 12, 0], //图表内边距
+  }
+```
+
+* data:
+
+```javascript
+[
+  {
+    name: '机房1',
+    data: [
+      [1483372800000, 1892],
+      [1483459200000, 7292],
+      [1483545600000, 5714],
+      [1483632000000, 5354],
+      [1483718400000, 2014],
+      [1483804800000, 22],
+      [1483891200000, 11023],
+      [1483977600000, 5218],
+      [1484064000000, 8759],
+      [1484150400000, 9981],
+      [1484236800000, 4533],
+      [1484323200000, 11398],
+      [1484409600000, 1064],
+      [1484496000000, 6494]
+    ]
+  }
+];
+```
