a531bbef309e54f882495d3c3ccb04d30a3e8566
业枫 <zhaoye.zzy@alibaba-inc.com>
2018-05-24 10:40:35 +0800

    feat: 添加aone主题包

---
+++ components/theme/aone.js
@@ -0,0 +1,46 @@
+import color from '!!sass-variable-loader!@alife/aisc-core/scss/variables/_color.scss';
+import fonts from '!!sass-variable-loader!@alife/aisc-core/scss/variables/_font.scss';
+import size from '!!sass-variable-loader!@alife/aisc-core/scss/variables/_size.scss';
+
+import widgetsVar from '!!sass-variable-loader!./aone.scss';
+
+const prefix = 'widgets-';
+
+Object.assign(color, widgetsVar, {
+  [`${prefix}tooltip-background`]: 'rgba(255, 255, 255, 0.9)',
+  [`${prefix}tooltip-shadow`]: '0 1px 5px 0 rgba(0, 0, 0, 0.2)',
+  [`${prefix}tooltip-cross-line`]: '#dddddd',
+  [`${prefix}tooltip-cross-react`]: '#CCD6EC',
+  [`${prefix}tooltip-cross-react-opacity`]: 0.3,
+
+  category_12: [
+    widgetsVar.widgetsColorCategory1,
+    widgetsVar.widgetsColorCategory2,
+    widgetsVar.widgetsColorCategory3,
+    widgetsVar.widgetsColorCategory4,
+    widgetsVar.widgetsColorCategory5,
+    widgetsVar.widgetsColorCategory6,
+    widgetsVar.widgetsColorCategory7,
+    widgetsVar.widgetsColorCategory8,
+    widgetsVar.widgetsColorCategory9,
+    widgetsVar.widgetsColorCategory10,
+    widgetsVar.widgetsColorCategory11,
+    widgetsVar.widgetsColorCategory12,
+  ],
+  linear_10: [
+    widgetsVar.widgetsColorLinear1,
+    widgetsVar.widgetsColorLinear5,
+    widgetsVar.widgetsColorLinear9,
+    widgetsVar.widgetsColorLinear3,
+    widgetsVar.widgetsColorLinear7,
+    widgetsVar.widgetsColorLinear10,
+    widgetsVar.widgetsColorLinear2,
+    widgetsVar.widgetsColorLinear4,
+    widgetsVar.widgetsColorLinear6,
+    widgetsVar.widgetsColorLinear8,
+  ],
+});
+
+fonts.fontFamilyNumber = widgetsVar.fontFamilyNumber;
+
+export { size, color, fonts };
