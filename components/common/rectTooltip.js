c6f7c1896de8eeda46f63e41802f0e4ba3ff2585
业枫 <zhaoye.zzy@alibaba-inc.com>
2018-06-01 15:18:21 +0800

    feat: 添加 tooltip 排序功能，补充注释

--- components/common/rectTooltip.js
+++ components/common/rectTooltip.js
@@ -1,12 +1,24 @@
 'use strict';
 
+import G2 from '@antv/g2';
 import { getRawData } from './common';
+
+// 排序函数
+const sortFun = {
+  // 升序
+  asce(a, b) {
+    return a.value - b.value;
+  },
+  // 降序
+  desc(a, b) {
+    return b.value - a.value;
+  },
+};
 
 /*
 * 常见直角坐标系的tooltip，包含title、name、value
 * */
 export default function (chart, config, customConfig) {
-  // tooltip
   if (config.tooltip) {
     const tooltipCfg = {
       // crosshairs 空对象不可省略，否则在混合图表中会没有crosshairs line
@@ -19,20 +31,31 @@ export default function (chart, config, customConfig) {
       Object.assign(tooltipCfg, customConfig);
     }
     chart.tooltip(tooltipCfg);
-    if (config.tooltip.titleFormatter || config.tooltip.nameFormatter || config.tooltip.valueFormatter) {
+
+    const { sort, titleFormatter, nameFormatter, valueFormatter } = config.tooltip;
+    if (sort || titleFormatter || nameFormatter || valueFormatter) {
       chart.on('tooltip:change', (ev) => {
-        if (config.tooltip.titleFormatter) {
-          ev.items[0].title = config.tooltip.titleFormatter(ev.items[0].title, ev.items);
+        // 如果设置了合法的排序关键字，则开始排序
+        if (G2.Util.isFunction(sort)) {
+          ev.items.sort(sort);
+        } else if (sortFun[sort]) {
+          ev.items.sort(sortFun[sort]);
         }
 
+        // 格式化标题
+        if (titleFormatter) {
+          ev.items[0].title = titleFormatter(ev.items[0].title, ev.items);
+        }
+
+        // 对每一项格式化 名字 和 值
         ev.items.forEach((item, index) => {
           const raw = getRawData(config, this.rawData, item);
 
-          if (config.tooltip.valueFormatter) {
-            item.value = config.tooltip.valueFormatter(item.value, raw, index, ev.items);
+          if (valueFormatter) {
+            item.value = valueFormatter(item.value, raw, index, ev.items);
           }
-          if (config.tooltip.nameFormatter) {
-            item.name = config.tooltip.nameFormatter(item.name, raw, index, ev.items);
+          if (nameFormatter) {
+            item.name = nameFormatter(item.name, raw, index, ev.items);
           }
         });
       });
