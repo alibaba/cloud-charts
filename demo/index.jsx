f886df71d7b9589d02bab3f32b9f7e31f8a7bde5
业枫 <zhaoye.zzy@alibaba-inc.com>
2018-11-16 16:47:54 +0800

    refactor: 去除冗余代码

--- demo/index.jsx
+++ demo/index.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import * as Widgets from '@alife/aisc-widgets';
 
-console.log(Widgets);
-
 class Demo extends React.Component {
   render() {
     return (
