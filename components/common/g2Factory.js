3817c62220054ef6cfd716c3594682a6007dc233
业枫 <zhaoye.zzy@alibaba-inc.com>
2019-03-14 14:36:04 +0800

    refactor: G2基类减少多余props

--- components/common/g2Factory.js
+++ components/common/g2Factory.js
@@ -302,7 +302,7 @@ function g2Factory(name, Chart, convertData = true) {
     }
 
     render() {
-      const { className = '', style, children, data, width, height, padding, config, ...otherProps } = this.props;
+      const { className = '', style, children, data, width, height, padding, config, animate, language, customChart, ...otherProps } = this.props;
       return (
         <div ref={dom => (this.chartDom = dom)} id={this.chartId} key={this.chartId} className={`${rootClassName + name} ${className}`} style={style} {...otherProps}>
           {children ? <div className={rootChildClassName}>{children}</div> : null}
