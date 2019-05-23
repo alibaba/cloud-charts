e4795274d62632bd1b7ff14475a0940639a83e9e
业枫 <zhaoye.zzy@alibaba-inc.com>
2019-05-23 16:32:01 +0800

    feat: minicontainer 去除传递到div上的 isMobile 属性

--- components/wminicontainer/index.jsx
+++ components/wminicontainer/index.jsx
@@ -25,10 +25,10 @@ export default class Wminicontainer extends React.Component {
   }
 
   render() {
-    const { height = 80, className, status, style, ...otherProps } = this.props;
+    const { height = 80, className, status, style, isMobile, ...otherProps } = this.props;
     const mainClasses = classNames({
       [`${prefix}`]: true,
-      [`${prefix}-mobile`]: isMobileWithProps(otherProps),
+      [`${prefix}-mobile`]: isMobileWithProps(otherProps, isMobile),
       [`${prefix}-${getStatusColorName(status)}`]: !!status,
       [className]: !!className
     });
