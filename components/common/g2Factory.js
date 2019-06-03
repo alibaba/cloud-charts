bd0cf94c2c61787fb52eaa2aabaefeb5580dbcfd
业枫 <zhaoye.zzy@alibaba-inc.com>
2019-06-03 11:22:16 +0800

    fix: 修复图表外层存在css scale时tooltip位置不正确的问题。

--- components/common/g2Factory.js
+++ components/common/g2Factory.js
@@ -10,6 +10,28 @@ import chartLog from './log';
 
 setG2Theme(G2);
 
+/**
+ * 在G2初始化前，替换 G.Canvas.getPointByClient 函数，适配CSS缩放的场景。
+ * */
+const rawGetPointByClient = G2.G.Canvas.prototype.getPointByClient;
+// 由于需要运行时this指针，这个函数不可改为箭头函数。
+G2.G.Canvas.prototype.getPointByClient = function(clientX, clientY) {
+  // 获取原函数返回的坐标值
+  const raw = rawGetPointByClient.call(this, clientX, clientY);
+  // 获取设定高宽和真实高宽。
+  // 当设定的高宽不等于getBoundingClientRect获取的高宽时，说明存在缩放。
+  const el = this.get('el');
+  const bbox = el.getBoundingClientRect();
+  const setWidth = this.get('width');
+  const setHeight = this.get('height');
+  const { width: realWidth, height: realHeight } = bbox;
+  // 除以缩放比（真实高宽 / 设定高宽）获得真实的坐标。
+  return {
+    x: raw.x / (realWidth / setWidth),
+    y: raw.y / (realHeight / setHeight),
+  };
+};
+
 // 图表唯一id
 let uniqueId = 0;
 function generateUniqueId() {
