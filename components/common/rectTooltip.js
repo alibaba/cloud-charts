71b903b23735b744271ea9cb4d0d998bf0ce8679
业枫 <zhaoye.zzy@alibaba-inc.com>
2020-03-16 16:58:46 +0800

    chore: 升级G2版本

--- components/common/rectTooltip.js
+++ components/common/rectTooltip.js
@@ -41,7 +41,7 @@ export default function(chart, config, componentConfig) {
       inPlot,
       itemTpl: !config.isCandlestick
         ? `<li data-index={index}>
-        <span style="background-color:{color};" class="g2-tooltip-marker"></span>
+        <svg viewBox="0 0 6 6" class="g2-tooltip-marker"></svg>
         <span class="g2-tooltip-item-name">{name}</span>${
           showColon ? ':' : ''
         }<span class="g2-tooltip-item-value">{value}</span>
