a140f8412a8988712cfbcc3dd19629b3a8c332d8
业枫 <zhaoye.zzy@alibaba-inc.com>
2020-07-23 20:19:38 +0800

    chore: d.ts 增加 any 声明

--- index.d.ts
+++ index.d.ts
@@ -8,11 +8,11 @@ declare namespace AiscWidgets {
 
     // 主题包和颜色值
     export namespace themes {
-        function setTheme(): void;
+        function setTheme(theme: string): void;
     }
 
     // 打点控制
-    export function track(): void;
+    export function track(enable: boolean): void;
 
     // 插件系统
     export const plugins: object;
@@ -21,48 +21,48 @@ declare namespace AiscWidgets {
     // 暴露工具类
     export namespace Util {
         const propertyMap: object;
-        function propertyAssign();
-        function getParentSize();
-        function getStatusColor();
-        function isInvalidNumber();
-        function numberDecimal();
-        function beautifyNumber();
-        function getRawData();
-        function filterKey();
-        function isEqual();
-        function isEqualWith();
-        function merge();
+        function propertyAssign(...args: any[]): any;
+        function getParentSize(...args: any[]): any;
+        function getStatusColor(...args: any[]): any;
+        function isInvalidNumber(...args: any[]): any;
+        function numberDecimal(...args: any[]): any;
+        function beautifyNumber(...args: any[]): any;
+        function getRawData(...args: any[]): any;
+        function filterKey(...args: any[]): any;
+        function isEqual(...args: any[]): any;
+        function isEqualWith(...args: any[]): any;
+        function merge(...args: any[]): any;
     }
 
-    export const Wnumber: React.Component;
-    export const Wcontainer: React.Component;
-    export const Wicon: React.Component;
-    export const Wcircle: React.Component;
-    export const Wminicontainer: React.Component;
-    export const Wshoot: React.Component;
-    export const Wplaceholder: React.Component;
-    export const Wperline: React.Component;
-    export const Wcount: React.Component;
-    export const Wdashboard: React.Component;
+    export const Wnumber: React.Component<any>;
+    export const Wcontainer: React.Component<any>;
+    export const Wicon: React.Component<any>;
+    export const Wcircle: React.Component<any>;
+    export const Wminicontainer: React.Component<any>;
+    export const Wshoot: React.Component<any>;
+    export const Wplaceholder: React.Component<any>;
+    export const Wperline: React.Component<any>;
+    export const Wcount: React.Component<any>;
+    export const Wdashboard: React.Component<any>;
 
-    export const Wline: React.Component;
-    export const Wminiline: React.Component;
-    export const Wbar: React.Component;
-    export const Whistogram: React.Component;
-    export const Wlinebar: React.Component;
-    export const Wpie: React.Component;
-    export const Wscatter: React.Component;
-    export const Wmap: React.Component;
-    export const Wcustom: React.Component;
-    export const Wsankey: React.Component;
-    export const Wnightingale: React.Component;
-    export const Wradar: React.Component;
-    export const Wrectangle: React.Component;
-    export const Wfunnel: React.Component;
-    export const WmultiPie: React.Component;
-    export const Wbox: React.Component;
-    export const Wcandlestick: React.Component;
-    export const Wheatmap: React.Component;
-    export const Wtreemap: React.Component;
-    export const Whierarchy: React.Component;
+    export const Wline: React.Component<any>;
+    export const Wminiline: React.Component<any>;
+    export const Wbar: React.Component<any>;
+    export const Whistogram: React.Component<any>;
+    export const Wlinebar: React.Component<any>;
+    export const Wpie: React.Component<any>;
+    export const Wscatter: React.Component<any>;
+    export const Wmap: React.Component<any>;
+    export const Wcustom: React.Component<any>;
+    export const Wsankey: React.Component<any>;
+    export const Wnightingale: React.Component<any>;
+    export const Wradar: React.Component<any>;
+    export const Wrectangle: React.Component<any>;
+    export const Wfunnel: React.Component<any>;
+    export const WmultiPie: React.Component<any>;
+    export const Wbox: React.Component<any>;
+    export const Wcandlestick: React.Component<any>;
+    export const Wheatmap: React.Component<any>;
+    export const Wtreemap: React.Component<any>;
+    export const Whierarchy: React.Component<any>;
 }
