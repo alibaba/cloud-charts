8a6b0943fcd46883014e42d414b91d9928b330df
Maplor <maplor@163.com>
2020-09-03 16:09:40 +0800

    chore: remove test

--- test/index.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import ConsoleCharts from '../src/index';
-import '../src/main.scss';
-
-it('renders', () => {
-  const wrapper = shallow(<ConsoleCharts />);
-  expect(wrapper.find('.ConsoleCharts').length).toBe(1);
-});
