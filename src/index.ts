082a4460cc23f5669d1291d1caab4e0f1404343c
Maplor <maplor@163.com>
2020-09-11 14:58:44 +0800

    feat: 暴露新增组件

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import '@alicloud/cloud-charts/build/index.css';
 
 export { default as LineBase } from './LineBase/index';
+export { default as LineArea } from './LineArea/index';
+export { default as LineAreaStack } from './LineAreaStack/index';
+export { default as LineSmooth } from './LineSmooth/index';
