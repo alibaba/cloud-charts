248ad10eda2ceb93cd0b869e0d3b649d9ac87d7b
业枫 <zhaoye.zzy@alibaba-inc.com>
2020-09-27 17:13:11 +0800

    style: 多余空格

--- src/Wline/index.tsx
+++ src/Wline/index.tsx
@@ -1,7 +1,7 @@
 'use strict';
 
 import { Chart, Types, BaseChartConfig } from '../common/types';
-import Base  from "../common/Base";
+import Base from "../common/Base";
 // import Brush from '@antv/g2-brush';
 // import g2Factory from '../common/g2Factory';
 // import errorWrap from '../common/errorWrap';
