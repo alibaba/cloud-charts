8ea4919af2b5ad0756e2ee2d2935ea52e96f7ecd
业枫 <zhaoye.zzy@alibaba-inc.com>
2020-10-09 19:29:48 +0800

    feat: 引入业务组件

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 import './index.scss';
 
 export { default as Wline } from './Wline';
+
+// 业务组件，没有依赖其它图表库
+export { default as Wnumber } from './Wnumber/index';
+export { default as Wcontainer } from './Wcontainer/index';
+export { default as Wicon } from './Wicon/index';
+export { default as Wcircle } from './Wcircle/index';
+export { default as Wminicontainer } from './Wminicontainer/index';
+export { default as Wshoot } from './Wshoot/index';
+export { default as Wplaceholder } from './Wplaceholder/index';
+export { default as Wcount } from './Wcount/index';
+export { default as Wdashboard } from './Wdashboard/index';
