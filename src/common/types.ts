e3f5105236c1bf9ec7723f9bda7d7d2429d129e8
业枫 <zhaoye.zzy@alibaba-inc.com>
2020-10-09 19:00:50 +0800

    feat: 增加 types 内容

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,5 +1,7 @@
 export * from '@antv/g2/lib/core';
 
+export * as G2Dependents from "@antv/g2/lib/dependents";
+
 export interface BaseChartConfig {
   padding?: string[];
   dataType?: string;
@@ -8,3 +10,7 @@ export interface BaseChartConfig {
 export type ChartData = any;
 
 export type Size = number | string;
+
+export type Status = 'error' | 'warning' | 'normal' | 'success' | 'none';
+
+export type StatusColor = 'red' | 'orange' | 'blue' | 'green' | 'gray';
