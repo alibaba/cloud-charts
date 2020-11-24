ebd8aa94356092eaaee109b6a391a86828fa77ed
业枫 <zhaoye.zzy@alibaba-inc.com>
2020-11-24 23:16:23 +0800

    chore: 构建时预先编译 主题包scss变量 到 [theme].style.js

--- build.before.js
+++ build.before.js
@@ -1,7 +1,28 @@
+const path = require('path');
+const fs = require('fs');
+const sassExtract = require('sass-extract');
+const createSassExtractJsPlugin = require('./sass-extract-js/plugin');
+const sassExtractJsPlugin = createSassExtractJsPlugin({ camelCase: false, hex: true });
 const packageInfo = require('./package.json');
 
 /** 自定义构建脚本 - 前置 */
-module.exports = ({ modifyUserConfig }) => {
+module.exports = ({ context, log, modifyUserConfig }) => {
+  const { rootDir } = context;
+  // 编译 主题包scss变量 到 [theme].style.js
+  const themePath = path.resolve(rootDir, 'src/themes');
+  fs.readdirSync(themePath).forEach((theme) => {
+    if (/\w+.scss$/.test(theme) && theme !== 'base.scss' && theme !== 'index.scss') {
+      const rendered = sassExtract.renderSync({
+        file: themePath + '/' + theme,
+      }, {
+        plugins: [sassExtractJsPlugin]
+      });
+
+      fs.writeFileSync(themePath + '/' + theme.replace(/\.scss$/, '.style.js'), `export default ${JSON.stringify(rendered.vars)};`);
+
+      log.info(`build theme: ${theme}`);
+    }
+  });
 
   // 自定义babel插件
   modifyUserConfig('babelPlugins', [
