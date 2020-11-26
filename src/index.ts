5798621c0854500a1ebba7dff69673a1e1acd99d
业枫 <zhaoye.zzy@alibaba-inc.com>
2020-11-26 17:54:16 +0800

    feat: 暴露版本号、主题、工具类、插件系统

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
+import { VERSION } from './constants';
+import themes from './themes/index';
 import './index.scss';
+
+export const version = VERSION;
+export { VERSION } ;
+
+// 主题包和颜色值
+export const COLORS = themes;
+export const THEMES = themes;
+export { themes };
+
+// 打点控制
+export { default as track } from './track';
+
+// 插件系统
+export { plugins, pluginManager } from './plugins';
+
+// 暴露工具类
+export { default as Util } from './Util';
 
 export { default as Wline } from './Wline';
 export { default as Wlinebar } from './Wlinebar';
