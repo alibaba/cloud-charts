abf7eeaf181833addca4dcd12afc34a258231dd8
业枫 <zhaoye.zzy@alibaba-inc.com>
2020-12-01 15:54:15 +0800

    test: 调整漏斗图示例

--- stories/chart.funnel.stories.js
+++ stories/chart.funnel.stories.js
@@ -20,3 +20,92 @@ stories.addDecorator(withKnobs);
 stories.add('漏斗图', () => (
     <Wfunnel height="300" data={data} />
 ));
+
+const directionData = [
+  {
+    "name":"柱1",
+    "data":[["一", 100], ["二", 94], ["三", 86], ["四", 71], ["五", 67], ["六", 54]]
+  }
+];
+stories.add('纵向-左侧', () => (
+  <Wcontainer className="demos">
+    <Wfunnel
+      width={500}
+      height="300"
+      config={{
+        direction: 'vertical',
+        align: 'left',
+      }}
+      data={directionData}
+    />
+  </Wcontainer>
+));
+stories.add('纵向-中间', () => (
+  <Wcontainer className="demos">
+    <Wfunnel
+      width={500}
+      height="300"
+      config={{
+        padding: [20, 20, 20, 20],
+        direction: 'vertical',
+        align: 'center',
+        label: true,
+      }}
+      data={directionData}
+    />
+  </Wcontainer>
+));
+stories.add('纵向-右侧', () => (
+  <Wcontainer className="demos">
+    <Wfunnel
+      width={500}
+      height="300"
+      config={{
+        direction: 'vertical',
+        align: 'right',
+      }}
+      data={directionData}
+    />
+  </Wcontainer>
+));
+
+stories.add('横向-顶部', () => (
+  <Wcontainer className="demos">
+    <Wfunnel
+      width={500}
+      height="300"
+      config={{
+        direction: 'horizontal',
+        align: 'top',
+      }}
+      data={directionData}
+    />
+  </Wcontainer>
+));
+stories.add('横向-中间', () => (
+  <Wcontainer className="demos">
+    <Wfunnel
+      width={500}
+      height="300"
+      config={{
+        padding: [20, 20, 20, 20],
+        direction: 'horizontal',
+        align: 'center',
+      }}
+      data={directionData}
+    />
+  </Wcontainer>
+));
+stories.add('横向-底部', () => (
+  <Wcontainer className="demos">
+    <Wfunnel
+      width={500}
+      height="300"
+      config={{
+        direction: 'horizontal',
+        align: 'bottom',
+      }}
+      data={directionData}
+    />
+  </Wcontainer>
+));
