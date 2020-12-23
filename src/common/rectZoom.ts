7d51e3e534f56b425081e1685649af9fde20fbb9
业枫 <zhaoye.zzy@alibaba-inc.com>
2020-12-23 11:44:50 +0800

    feat: 增加zoom缩放事件

--- src/common/rectZoom.ts
+++ src/common/rectZoom.ts
@@ -21,6 +21,10 @@ function isNotInResetButton(context: Types.IInteractionContext) {
   return !(context.isInShape('button-rect') || context.isInShape('button-text'));
 }
 
+function isPointInViewNotInResetButton(context: Types.IInteractionContext) {
+  return context.isInPlot() && !(context.isInShape('button-rect') || context.isInShape('button-text'));
+}
+
 // registerAction('custom-reset-button', ResetButton, {
 //   name: 'reset-button',
 //   text: 'reset',
@@ -64,13 +68,26 @@ export default function(chart: Chart, config: ZoomConfig, language?: Language) {
   });
 
   chart.interaction('brush-x', {
+    start: [
+      {
+        trigger: 'mousedown',
+        isEnable: isPointInViewNotInResetButton,
+        action: ['brush-x:start', 'x-rect-mask:start', 'x-rect-mask:show'],
+        callback(context: Types.IInteractionContext) {
+          // console.log('start', context, );
+          chart.emit('zoom:start', {
+            ...(context.getCurrentPoint())
+          }, context);
+        }
+      }
+    ],
     end: [
       {
         trigger: 'mouseup',
         isEnable(context: Types.IInteractionContext) {
-          const isInPlot = context.isInPlot();
+          const enable = isPointInViewNotInResetButton(context);
           // 自定义设置重置按钮的文案
-          if (isInPlot) {
+          if (enable) {
             const resetAction = context.getAction('reset-button');
             // @ts-ignore
             if (resetAction.cfg) {
@@ -79,14 +96,31 @@ export default function(chart: Chart, config: ZoomConfig, language?: Language) {
               // TODO 重置按钮根据主题设置样式
             }
           }
-          return isInPlot;
+          return enable;
         },
         action: ['brush-x:filter', 'brush-x:end', 'x-rect-mask:end', 'x-rect-mask:hide', 'reset-button:show'],
+        callback(context: Types.IInteractionContext) {
+          const rangeFilterAction = context.getAction('brush-x');
+
+          chart.emit('zoom:end', {
+            // @ts-ignore
+            startPoint: rangeFilterAction.startPoint,
+            endPoint: context.getCurrentPoint(),
+            // @ts-ignore
+            data: context.view.filteredData,
+          }, context);
+        }
       },
     ],
     rollback: [
       // { trigger: 'dblclick', action: ['brush-x:reset'] },
-      {trigger: 'reset-button:click', action: ['brush-x:reset', 'reset-button:hide', 'cursor:crosshair']}
+      {
+        trigger: 'reset-button:click',
+        action: ['brush-x:reset', 'reset-button:hide', 'cursor:crosshair'],
+        callback(context: Types.IInteractionContext) {
+          chart.emit('zoom:reset', context);
+        }
+      }
     ],
   });
 }
