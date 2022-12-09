6529b2decd3dc851489a48e574e7efb4b633b069
lucy-cl <cl19940816@163.com>
2022-12-09 10:51:19 +0800

    refactor: 优化空数据检测与处理

--- src/common/checkFunctions.ts
+++ src/common/checkFunctions.ts
@@ -1,14 +1,19 @@
+import EmptyDataType, { EmptyJudgeType } from './emptyDataType';
+
 // 空数据检测
-export function checkEmptyData(data: any) {
-  console.log('data check:', data);
-  if (!data) {
-    return true;
-  }
-  if (Array.isArray(data)) {
+export function checkEmptyData(data: any, chartType: string) {
+  const type = (EmptyDataType as any)[chartType]?.emptyJudge;
+  if (type === EmptyJudgeType.COMMON) {
     return data?.length === 0 || data?.every((item: any) => !item?.data || item?.data?.length === 0);
   }
-  if (typeof data === 'object') {
-    return Object.keys(data).length === 0 || data?.children?.length === 0;
+  if (type === EmptyJudgeType.CHILDREN) {
+    return !data?.children || data?.children?.length === 0;
+  }
+  if (type === EmptyJudgeType.ARRAY) {
+    return data?.length === 0 || data?.every((item: any) => item?.x === undefined || item?.y === undefined);
+  }
+  if (type === EmptyJudgeType.GRAPH) {
+    return !data?.nodes || data?.nodes?.length === 0 || !data?.links || data?.links?.length === 0;
   }
 
   return false;
--- src/common/emptyDataType.ts
+++ src/common/emptyDataType.ts
@@ -1,49 +1,233 @@
-export const ProcessType = {
-  RECT: 'RECT', // 直角坐标系
-  POLAR: 'POLAR', // 极坐标系
-  RADAR: 'RADAR', // 雷达图
-  OTHERS: 'OTHERS', // 其他
+/**
+ * 判断是否无数据的方法分类
+ *  COMMON    {name: xxx, data: [xxx]}
+ *  ARRAY    [{x: ... , y: ...}]
+ *  CHILDREN  { children: [xxx]}
+ *  GRAPH    { nodes: [xxx], links: [xxx]}
+ */
+export const EmptyJudgeType = {
+  COMMON: 'COMMON',
+  ARRAY: 'ARRAY',
+  CHILDREN: 'CHILDREN',
+  GRAPH: 'GRAPH',
 };
 
 const EmptyDataType = {
   // 柱状图
-  G2Bar: ProcessType.RECT,
+  G2Bar: {
+    emptyJudge: EmptyJudgeType.COMMON, // 检测空数据的方式
+    replacement: {
+      // 空状态下替换的数据与配置项
+      data: [],
+      config: {
+        xAxis: {
+          min: 0,
+          max: 100,
+          label: null,
+        },
+        yAxis: {
+          min: 0,
+          max: 100,
+          label: null,
+        },
+      },
+    },
+  },
   // 箱型图
-  G2Box: ProcessType.RECT,
+  G2Box: {
+    emptyJudge: EmptyJudgeType.COMMON,
+    replacement: {
+      data: [],
+      config: {
+        xAxis: {
+          min: 0,
+          max: 100,
+          label: null,
+        },
+        yAxis: {
+          min: 0,
+          max: 100,
+          label: null,
+        },
+      },
+    },
+  },
   // 烛形图
-  G2Wcandlestick: ProcessType.RECT,
+  G2Wcandlestick: {
+    emptyJudge: EmptyJudgeType.COMMON,
+    replacement: {
+      data: [],
+      config: {
+        xAxis: {
+          min: 0,
+          max: 100,
+          label: null,
+        },
+        yAxis: {
+          min: 0,
+          max: 100,
+          label: null,
+        },
+      },
+    },
+  },
   // 漏斗图
-  G2Funnel: ProcessType.OTHERS,
+  G2Funnel: {
+    emptyJudge: EmptyJudgeType.COMMON,
+    replacement: {},
+  },
   // 热力图
-  G2Heatmap: ProcessType.OTHERS,
+  G2Heatmap: {
+    emptyJudge: EmptyJudgeType.COMMON,
+    replacement: {},
+  },
   // 层次图
-  G2Hierarchy: ProcessType.OTHERS,
+  G2Hierarchy: {
+    emptyJudge: EmptyJudgeType.CHILDREN,
+    replacement: {},
+  },
   // 直方图
-  G2Histogram: ProcessType.RECT,
+  G2Histogram: {
+    emptyJudge: EmptyJudgeType.COMMON,
+    replacement: {
+      data: [],
+      config: {
+        xAxis: {
+          min: 0,
+          max: 100,
+          label: null,
+        },
+        yAxis: {
+          min: 0,
+          max: 100,
+          label: null,
+        },
+      },
+    },
+  },
   // 线图
-  G2Line: ProcessType.RECT,
+  G2Line: {
+    emptyJudge: EmptyJudgeType.COMMON,
+    replacement: {
+      data: [],
+      config: {
+        xAxis: {
+          min: 0,
+          max: 100,
+          label: null,
+        },
+        yAxis: {
+          min: 0,
+          max: 100,
+          label: null,
+        },
+      },
+    },
+  },
   // 线柱图
-  G2LineBar: ProcessType.RECT,
+  G2LineBar: {
+    emptyJudge: EmptyJudgeType.COMMON,
+    replacement: {},
+  },
   // 线点图
-  G2LineScatter: ProcessType.RECT,
+  G2LineScatter: {
+    emptyJudge: EmptyJudgeType.COMMON,
+    replacement: {},
+  },
   // 地图
-  G2Map: ProcessType.OTHERS,
+  // G2Map: {
+  //   emptyJudge: EmptyJudgeType.COMMON,
+  //   replacement: {},
+  // },
   // 多重饼图
-  G2MultiPie: ProcessType.POLAR,
+  G2MultiPie: {
+    emptyJudge: EmptyJudgeType.CHILDREN,
+    replacement: {
+      data: {
+        children: [
+          {
+            name: 'root',
+            value: 100,
+          },
+        ],
+      },
+      config: {
+        legend: false,
+        colors: '#F7F7F7',
+      },
+    },
+  },
   // 玫瑰图
-  G2Nightingale: ProcessType.POLAR,
+  G2Nightingale: {
+    emptyJudge: EmptyJudgeType.COMMON,
+    replacement: {},
+  },
   // 饼图
-  G2Pie: ProcessType.POLAR,
+  G2Pie: {
+    emptyJudge: EmptyJudgeType.COMMON,
+    replacement: {}, // 饼图无需替换数据与配置项
+  },
   // 雷达图
-  G2Radar: ProcessType.RADAR,
+  G2Radar: {
+    emptyJudge: EmptyJudgeType.COMMON,
+    replacement: {
+      data: [
+        { x: 'a', y: 0 },
+        { x: 'b', y: 0 },
+        { x: 'c', y: 0 },
+        { x: 'd', y: 0 },
+        { x: 'e', y: 0 },
+        { x: 'f', y: 0 },
+      ],
+      config: {
+        xAxis: {
+          label: null,
+        },
+        yAxis: {
+          min: 0,
+          max: 100,
+          label: null,
+        },
+        legend: false,
+        tooltip: false,
+        colors: '#e6e6e6',
+      },
+    },
+  },
   // 分箱图
-  G2Rectangle: ProcessType.OTHERS,
+  G2Rectangle: {
+    emptyJudge: EmptyJudgeType.ARRAY,
+    replacement: {},
+  },
   // 桑基图
-  G2Sankey: ProcessType.OTHERS,
+  G2Sankey: {
+    emptyJudge: EmptyJudgeType.GRAPH,
+    replacement: {},
+  },
   // 散点图
-  G2Scatter: ProcessType.RECT,
+  G2Scatter: {
+    emptyJudge: EmptyJudgeType.COMMON,
+    replacement: {
+      data: [],
+      config: {
+        xAxis: {
+          min: 0,
+          max: 100,
+          label: null,
+        },
+        yAxis: {
+          min: 0,
+          max: 100,
+          label: null,
+        },
+      },
+    },
+  },
   // 树图
-  G2Treemap: ProcessType.OTHERS,
+  G2Treemap: {
+    emptyJudge: EmptyJudgeType.CHILDREN,
+    replacement: {},
+  },
 };
 
 export default EmptyDataType;
