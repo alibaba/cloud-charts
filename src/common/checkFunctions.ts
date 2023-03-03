0a821890f610602c91adee734eb9ee284ed28471
陈璐 <lucy.cl@alibaba-inc.com>
2023-03-03 11:35:51 +0800

    refactor: 优化柱图极端数据左对齐方案

--- src/common/checkFunctions.ts
+++ src/common/checkFunctions.ts
@@ -259,7 +259,11 @@ export function checkExtremeData(
               },
             })),
           },
-          xAxis,
+          xAxis: {
+            ...xAxis,
+            autoHide: false,
+            autoEllipsis: true
+          },
         },
       };
     }
