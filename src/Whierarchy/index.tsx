c6e4b79da6f78570eacc108cda46c201ecd11617
陈璐 <lucy.cl@alibaba-inc.com>
2023-03-07 20:22:15 +0800

    fix: Whierarchy 在 npm 引入时可能报错的问题

--- src/Whierarchy/index.tsx
+++ src/Whierarchy/index.tsx
@@ -1,7 +1,7 @@
 'use strict';
 import { View } from '@antv/data-set/lib/view';
 import '@antv/data-set/lib/api/hierarchy';
-import '@antv/data-set/lib/transform/hierarchy/treemap';
+import '@antv/data-set/lib/transform/hierarchy/partition';
 import '@antv/data-set/lib/connector/hierarchy.js';
 import { Chart, Types, BaseChartConfig, Colors } from '../common/types';
 import Base from "../common/Base";
@@ -68,6 +68,7 @@ export class Hierarchy extends Base<WhierarchyConfig> {
   }
   init(chart: Chart, config: WhierarchyConfig, data: any) {
 
+    console.log(1111)
     const dataView = processDataView(data);
     const nodes = parseDataView(dataView);
     chart.data(nodes);
