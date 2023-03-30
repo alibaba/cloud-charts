994ae4c6e76be531d1d117c6bb1962c3ddbcef51
陈璐 <lucy.cl@alibaba-inc.com>
2023-03-30 17:48:47 +0800

    feat: 新增环状图底纹展示

--- stories/chart.multicircle.stories.js
+++ stories/chart.multicircle.stories.js
@@ -13,27 +13,43 @@ const test1 = [
     data: [
       ['一', 0.58],
       ['二', 0.23],
-      ['三', 0.19],
-      ['四', 0.27],
-      ['五', 1.29],
-      ['22', 0.27],
-      ['五22', 1.29],
+      // ['三', 0.19],
+      // ['四', 0.27],
+      // ['五', 1.29],
+      // ['22', 0.27],
+      // ['五22', 1.29],
     ],
   },
 ];
 
 stories.add('多重圆环', () => (
-  <Wcontainer className="demos">
-    <Wmulticircle
-      height="400"
-      config={{
-        max: 2 // 目前不可以小于所有数据中的最大值
-      }}
-      data={test1}
-    >
-      111
-    </Wmulticircle>
-  </Wcontainer>
+  <>
+    <Wcontainer className="demos">
+      <Wmulticircle
+        height="400"
+        config={{
+          max: 2, // 目前不可以小于所有数据中的最大值
+          showDecoration: true,
+        }}
+        data={test1}
+      >
+        111
+      </Wmulticircle>
+    </Wcontainer>
+    <Wcontainer className="demos">
+      <Wmulticircle
+        height="300"
+        config={{
+          max: 2, // 目前不可以小于所有数据中的最大值
+          showDecoration: {
+            innerRadius: 100, // 自定义圆环底
+          },
+          size: 16,
+        }}
+        data={test1}
+      >
+        <Wnumber>222</Wnumber>
+      </Wmulticircle>
+    </Wcontainer>
+  </>
 ));
-
-
