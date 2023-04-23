86c6f3bf7b8cd3208f38392c85d9c9827d6769a3
lucy-cl <cl19940816@163.com>
2023-04-23 10:46:41 +0800

    fix: 修复不支持的语言与文字国际化的问题

--- src/Wmap/index.tsx
+++ src/Wmap/index.tsx
@@ -852,8 +852,11 @@ function drawMapLabel(ctx: Map, chart: Chart, config: WmapConfig) {
 
   // 将背景数据集中的中心点坐标(cX, cY)映射为新数据中的x, y。保证scale可以同步这个view的度量。
   const labelData = ctx.bgMapDataView.rows.map((row) => {
+    console.log('row', row);
     const label = {
-      name: getText(row.properties.locale, ctx.context.language, ctx.context.locale),
+      name: row.properties.locale
+        ? getText(row.properties.locale, ctx.context.language, ctx.context.locale)
+        : row.properties.name || row.name,
       x: row.cX,
       y: row.cY,
     };
