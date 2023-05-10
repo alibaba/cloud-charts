347b92c37efba371ef1a45745da34a955c672c52
luhuihua <luhuihua.lhh@alibaba-inc.com>
2023-05-10 15:16:56 +0800

    feat: 增加颜色变量映射

---
+++ src/common/colorMap.ts
@@ -0,0 +1,37 @@
+export default {
+  // 状态色
+  disabled: '$widgets-color-gray',
+  normal: 'widgets-color-normal',
+  warning: 'widgets-color-orange',
+  error: 'widgets-color-red',
+  success: 'widgets-color-green',
+  p1: 'widgets-color-p1',
+  p2: 'widgets-color-p2',
+  p3: 'widgets-color-p3',
+  p4: 'widgets-color-p4',
+  p5: 'widgets-color-p5',
+  p6: 'widgets-color-p6',
+  p7: 'widgets-color-p7',
+
+  // 背景色
+  background: 'widgets-color-background',
+  'layout-background': 'widgets-color-layout-background',
+
+  // 分类色
+  'category-1': 'widgets-color-category-1',
+  'category-2': 'widgets-color-category-2',
+  'category-3': 'widgets-color-category-3',
+  'category-4': 'widgets-color-category-4',
+  'category-5': 'widgets-color-category-5',
+  'category-6': 'widgets-color-category-6',
+  'category-7': 'widgets-color-category-7',
+  'category-8': 'widgets-color-category-8',
+  'category-9': 'widgets-color-category-9',
+  'category-10': 'widgets-color-category-10',
+
+  // 文字颜色
+  'text-1': 'widgets-color-text-1',
+  'text-2': 'widgets-color-text-2',
+  'text-3': 'widgets-color-text-3',
+  'text-disable': 'widgets-color-disable',
+};
--- src/common/common.ts
+++ src/common/common.ts
@@ -5,6 +5,7 @@ import { warn } from './log';
 import isEqual from 'lodash/isEqual';
 import isEqualWith from 'lodash/isEqualWith';
 import merge from 'lodash/merge';
+import colorMap from './colorMap';
 export { isEqual, isEqualWith, merge };
 
 export const requestAnimationFrame = window && window.requestAnimationFrame;
@@ -166,26 +167,11 @@ export function getDataIndexColor(colors: Colors, rawData: any[], dataIndex: num
 /** 主题关键字 */
 /** 颜色映射, 主要针对状态色 */
 export function mapColors(colors: Colors) {
-  const colorMap: any = {
-    disabled: '$widgets-color-gray',
-    normal: 'widgets-color-normal',
-    warning: 'widgets-color-orange',
-    error: 'widgets-color-red',
-    success: 'widgets-color-green',
-    p1: 'widgets-color-p1',
-    p2: 'widgets-color-p2',
-    p3: 'widgets-color-p3',
-    p4: 'widgets-color-p4',
-    p5: 'widgets-color-p5',
-    p6: 'widgets-color-p6',
-    p7: 'widgets-color-p7',
-  };
-
   let newColors = colors;
   if (typeof colors === 'string') {
-    newColors = colors in colorMap ? (themes as any)[colorMap[colors]] : colors;
+    newColors = colors in colorMap ? (themes as any)[(colorMap as any)[colors]] : colors;
   } else if (Array.isArray(colors)) {
-    newColors = colors.map((color: string) => (color in colorMap ? (themes as any)[colorMap[color]] : color));
+    newColors = colors.map((color: string) => (color in colorMap ? (themes as any)[(colorMap as any)[color]] : color));
   }
 
   return newColors;
