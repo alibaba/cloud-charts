1afb33c50189f1f9380c3d5a382ea36fc7d151d9
luhuihua <luhuihua.lhh@alibaba-inc.com>
2023-08-24 14:21:39 +0800

    fix: 地图背景色增加渐变考虑

--- src/Wmap/index.tsx
+++ src/Wmap/index.tsx
@@ -257,7 +257,10 @@ export class Map extends Base<WmapConfig, MapProps> {
     if (config.showSouthChinaSea === undefined || config.showSouthChinaSea) {
       const { southChinaSeaKey } = this.state;
       const { fill } = config.background || {};
-      const mapColor = fill && !fill?.startsWith('p(a)data:image') ? fill : themes['widgets-map-area-bg'];
+      const mapColor =
+        fill && !fill?.startsWith('p(') && !fill.startsWith('l(') && !fill.startsWith('r(')
+          ? fill
+          : themes['widgets-map-area-bg'];
       const currentLanguage = getLanguage() || this.context.language;
 
       return (
