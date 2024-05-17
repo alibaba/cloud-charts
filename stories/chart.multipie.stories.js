54715f8b3159ddfca8d439a24e9a167ecd81f279
鹿梦 <lucy.cl@alibaba-inc.com>
2024-05-17 15:49:12 +0800

    refactor: 更新示例

--- stories/chart.multipie.stories.js
+++ stories/chart.multipie.stories.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import { storiesOf } from '@storybook/react';
 // import { action } from '@storybook/addon-actions';
 // import { linkTo } from '@storybook/addon-links';
